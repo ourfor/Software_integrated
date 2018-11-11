d6e655623a990604634fe75407c9233b47d9fcda
Sagit <ourfor@foxmail.com>
2018-11-11 11:26:35 +0800

    add branch alpha

---
+++ unit2/formatio.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[]){
+	printf("Nice to meet you\n");
+	int num_printf;
+	if(num_printf=printf("我打印的字符数为:")) printf("%d\n",num_printf);
+	return 0;
+}
